2e3e2efcedf1558d283cc0877f622aac57338a02
bharper3 <41131130+bharper3@users.noreply.github.com>
2020-04-13 22:06:38 -0500

    Add files via upload

---
+++ Kepler3/data/definitions.js
@@ -0,0 +1,9 @@
+/*
+This file contains question definitions used by a quiz.
+See http://astro.unl.edu/interactives for more information.
+Date published: 1/22/2019, 10:41:51 AM
+Editor version: Experimental, 21 January 2019
+*/
+
+questionDefinitions = {"f0eb6bc2-54bd-48a8-8b1b-bb6b2923f8c2":{"id":"f0eb6bc2-54bd-48a8-8b1b-bb6b2923f8c2","name":"KeplerThirdLaw","question":"The graphic shows four possible orbits for bodies traveling around the Sun. Notice that the coordinate system shows the distance from the Sun along each orbit in arbitrary units. Assume that the orbits are all in the same plane that you see here. Rank the orbits in order of increasing orbital period, from least to most.","bins":[{"id":"aa16ca91-e405-4785-9627-7374a36f4d04","items":[{"id":"a56ebd28-81dc-4487-8b39-373370f57c34","type":"image","value":"4","label":"","feedback":"a=4","src":"data/images/b0e57a5a-8a54-4a42-b2c7-10539794182a.png"},{"id":"cf8118db-f96f-4387-87f4-33696170b193","type":"image","value":"2","label":"","feedback":"a=3","src":"data/images/bf732f48-2ac0-4bba-9dc0-2cc72f2b50a1.png"},{"id":"aab05e0b-d547-44fe-8e48-030ad8a1b38a","type":"image","value":"3","label":"","feedback":"a=3.25","src":"data/images/c6fd7140-39ff-4da9-bd9e-82e2e280889f.png"},{"id":"2ba4a752-ec9e-4635-a523-dd621842ae26","type":"image","value":"1","label":"","feedback":"a=1.5","src":"data/images/b2218048-1d0c-4c96-bb58-5fca0cfcb892.png"}]}],"numToSelect":"4","background":{"src":"kepler3background.html"},"media":{"type":"image","src":"data/images/d2912856-733a-48ca-b59a-d3d6d8bb206f.png"}}};
+
