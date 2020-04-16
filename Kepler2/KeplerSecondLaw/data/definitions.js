5b657542152d33c12219fc1ca27c11e41a9a28de
bharper3 <41131130+bharper3@users.noreply.github.com>
2020-04-15 23:54:30 -0500

    Add files via upload

---
+++ Kepler2/KeplerSecondLaw/data/definitions.js
@@ -0,0 +1,9 @@
+/*
+This file contains question definitions used by a quiz.
+See http://astro.unl.edu/interactives for more information.
+Date published: 4/15/2020, 10:03:56 PM
+Editor version: Experimental - March 5, 2020
+*/
+
+questionDefinitions = {"979b164e-a448-405a-9cf6-f523ae031df0":{"id":"979b164e-a448-405a-9cf6-f523ae031df0","name":"KeplerSecondLaw","question":"The graphic shows the orbit of a body around the sun and the position of the body at four different times during its motion. Rank the given points of the planet's orbit in order of increasing orbital speed, from slowest to fastest.","bins":[{"id":"84747e60-ca4a-4b36-b36d-edd222df18c0","items":[{"id":"204ebd09-e80b-48f9-8493-696fb256b80d","type":"label","value":"1","feedback":"Farthest from Sun","x":0.04291418688501902,"y":0.48203592814371254},{"id":"8569ecf6-0ee2-4e33-9622-ebbf33227fe8","type":"label","value":"2","feedback":"Far from Sun","x":0.230538937384021,"y":0.14471051792898582},{"id":"fd1eac3b-7016-4a28-a061-ad93fb90d3ec","type":"label","value":"3","feedback":"Close to Sun","x":0.8512974661029503,"y":0.8113772455089823},{"id":"74366756-6d89-4f99-b90c-e4a991f5e48c","type":"label","value":"4","feedback":"Closest to Sun","x":0.949101735493856,"y":0.5638722554890221}]}],"numToSelect":"4","order":"lowToHigh","questionVariations":[],"background":{"src":"kepler2background.html"},"media":{"type":"labeledImage","itemSize":70,"src":"data/images/d6ee02cb-7e80-4939-89dc-b3d0d17ba86b.png"}}};
+
