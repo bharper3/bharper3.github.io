ccc097988006e048d7934f0564c6a2cfe57d2e5b
bharper3 <41131130+bharper3@users.noreply.github.com>
2020-04-15 23:58:44 -0500

    Add files via upload

---
+++ Kepler1/KeplerFirstLaw/data/definitions.js
@@ -0,0 +1,9 @@
+/*
+This file contains question definitions used by a quiz.
+See http://astro.unl.edu/interactives for more information.
+Date published: 4/15/2020, 10:03:55 PM
+Editor version: Experimental - March 5, 2020
+*/
+
+questionDefinitions = {"bb238659-03a7-4650-afd4-f02ea69560fe":{"id":"bb238659-03a7-4650-afd4-f02ea69560fe","name":"KeplerFirstLaw","question":"The graphic shows four possibilities for bodies moving in elliptical orbits around the Sun. The orbits are all in the same plane and the coordinate system shows the distance from the Sun in arbitrary units. Rank the orbits in order of increasing eccentricity, from least to most.","bins":[{"id":"ce06e5b7-7a03-4978-9bad-2302f7844be6","items":[{"id":"97dde996-50e2-4348-a425-1c250b60b908","type":"image","value":"2","feedback":"e=0.5","src":"data/images/3fab54ab-ed72-47b9-811f-5900c360563e.png"},{"id":"b344c304-8f15-4681-bc80-a05814ca5b2b","type":"image","value":"3","feedback":"e=0.67","src":"data/images/80662303-b8eb-4c38-ae8e-a38ba2a48abc.png"},{"id":"239d8695-2c59-4ec3-bb07-723de6187d30","type":"image","value":"1","feedback":"e=0","src":"data/images/f985e8a6-09d4-49fa-bbb3-5b1cc2b14805.png"},{"id":"86ffe818-39c0-48bd-bf0b-642510d7119b","type":"image","value":"4","feedback":"e=0.85","src":"data/images/c5012ac2-e879-4411-8108-dcbb1aeb3836.png"}]}],"numToSelect":"4","order":"lowToHigh","questionVariations":[],"background":{"src":"kepler1background.html"},"media":{"type":"image","src":"data/images/4dc752c9-5668-49b3-817d-7c7ab55f28d1.png"}}};
+
